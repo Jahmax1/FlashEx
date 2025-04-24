e387b05215617c299e3b76fd6a5ee3dd9d07988e
Jahmax <jahmaxsimba@gmail.com>
2025-04-24 20:58:14 +0300

    Set up Express backend with MongoDB

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+console.log('MONGODB_URI:', process.env.MONGODB_URI); // Debug log
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB error:', err));
+
+app.get('/', (req, res) => {
+  res.send('FlashEx Backend');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
