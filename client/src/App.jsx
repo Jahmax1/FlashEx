5d24f4485ac9b0fb5996c2bfb53c32379e2e5e29
Jahmax <jahmaxsimba@gmail.com>
2025-04-25 16:41:45 +0300

    Added navigation bar with role-based links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,21 @@ import UserDashboard from './pages/UserDashboard';
 import AdminDashboard from './pages/AdminDashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/buy-sell" element={<BuySell />} />
-      <Route path="/dashboard" element={<UserDashboard />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/buy-sell" element={<BuySell />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   );
 }
 
